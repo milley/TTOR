cab56edfe2ff885cfb61e087340412c9753386a4
milley <milley.gm@gmail.com>
2022-01-14 23:33:16 +0800

    ch11 generator feature in crossbeam

--- ch11/simple_crossbeam_select_demo/src/main.rs
+++ ch11/simple_crossbeam_select_demo/src/main.rs
@@ -1,43 +1,11 @@
-// use crossbeam_channel as channel;
-// use crossbeam_channel::{select, unbounded};
-// use std::thread;
+// use crossbeam::select;
+// use crossbeam::channel::unbounded;
 
-// fn fibonacci(fib: channel::Sender<u64>, quit: channel::Receiver<()>) {
-//     let (mut x, mut y) = (0, 1);
-//     loop {
-//         select! {
-//             send(fib, x) => {
-//                 let tmp = x;
-//                 x = y;
-//                 y = tmp + x;
-//             }
-//             recv(quit) => {
-//                 println!("quit");
-//                 return;
-//             }
-//         }
-//     }
-// }
-
-// fn main() {
-//     let (fib_s, fib_r) = channel::bounded(0);
-//     let (quit_s, quit_r) = channel::bounded(0);
-//     thread::spawn(move || {
-//         for _ in 0..10 {
-//             println!("{}", fib_r.recv().unwrap());
-//         }
-//         quit_s.send(());
-//     });
-//     fibonacci(fib_s, quit_r);
-// }
-
-
-// use crossbeam_channel::{select, unbounded};
 // fn main() {
 //     let (s1, r1) = unbounded();
 //     let (s2, r2) = unbounded();
 //     s1.send(10).unwrap();
-    
+
 //     // Since both operations are initially ready, a random one will be executed.
 //     select! {
 //         recv(r1) -> msg => assert_eq!(msg, Ok(10)),
@@ -48,6 +16,36 @@
 //     }
 // }
 
-fn main() {
-    
+use crossbeam::channel as channel;
+use crossbeam::{select, channel::unbounded};
+use std::thread;
+
+fn fibonacci(fib: channel::Sender<u64>, quit: channel::Receiver<()>) {
+    let (mut x, mut y) = (0, 1);
+    loop {
+        select! {
+            send(fib, x) -> _res => {
+                let tmp = x;
+                x = y;
+                y = tmp + x;
+            }
+            recv(quit) -> _msg => {
+                println!("quit");
+                return;
+            }
+        }
+    }
 }
+
+fn main() {
+    let (fib_s, fib_r) = channel::bounded(0);
+    let (quit_s, quit_r) = channel::bounded(0);
+    thread::spawn(move || {
+        for _ in 0..10 {
+            println!("{}", fib_r.recv().unwrap());
+        }
+        quit_s.send(());
+    });
+    fibonacci(fib_s, quit_r);
+}
+
